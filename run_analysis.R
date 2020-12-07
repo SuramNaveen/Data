142dab376f4d6f67112e42866dd0a906d0fe3f45
Naveen Suram <suramnaveen@gmail.com>
2020-12-07 22:36:18 +0530

    First draft

---
+++ run_analysis.R
@@ -0,0 +1,39 @@
+
+features <- read.table("UCI HAR Dataset/features.txt", col.names = c("n","functions"))
+activities_labels <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
+x_test <- read.table("UCI HAR Dataset/test/X_test.txt", col.names = features$functions)
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = "code")
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
+x_train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "code")
+
+X <- rbind(x_train, x_test)
+Y <- rbind(y_train, y_test)
+subject <- rbind(subject_train, subject_test)
+merged_data <- cbind(subject, Y, X)
+
+tidydata <- merged_data %>% select(subject, code, contains("mean"), contains("std"))
+
+tidydata$code <- activities[tidydata$code, 2]
+
+names(tidydata)[2] = "activity"
+names(tidydata)<-gsub("Acc", "Accelerometer", names(tidydata))
+names(tidydata)<-gsub("Gyro", "Gyroscope", names(tidydata))
+names(tidydata)<-gsub("BodyBody", "Body", names(tidydata))
+names(tidydata)<-gsub("Mag", "Magnitude", names(tidydata))
+names(tidydata)<-gsub("^t", "Time", names(tidydata))
+names(tidydata)<-gsub("^f", "Frequency", names(tidydata))
+names(tidydata)<-gsub("tBody", "TimeBody", names(tidydata))
+names(tidydata)<-gsub("-mean()", "Mean", names(tidydata), ignore.case = TRUE)
+names(tidydata)<-gsub("-std()", "STD", names(tidydata), ignore.case = TRUE)
+names(tidydata)<-gsub("-freq()", "Frequency", names(tidydata), ignore.case = TRUE)
+names(tidydata)<-gsub("angle", "Angle", names(tidydata))
+names(tidydata)<-gsub("gravity", "Gravity", names(tidydata))
+
+finaldata <- tidydata %>%
+  group_by(subject, activity) %>%
+  summarise_all(funs(mean))
+write.table(finaldata, "finaldata.txt", row.name=FALSE)
+
+str(finaldata)
